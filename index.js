fdfbf666d287e70f611c16eb069e9f1395fc0df4
pocho <rgfandres@gmail.com>
2020-10-07 19:40:21 +0200

    servidor node

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+//const spotifyAPI = require("./spotifyAPI");
+//const contactApi = require(path.join(__dirname,"contactApi"));
+const app = express();//modulo de api externo
+
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 80;
+
+//contactApi(app);
+//spotifyAPI(app);
+
+app.use("/",express.static("./public"));
+
+app.listen(port, () => {
+	console.log("Server ready");
+});
+
+console.log("Starting server...");
